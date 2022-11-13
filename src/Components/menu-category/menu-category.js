59d20fb5e65460dcaca6ee0dbadc0e2e3cdf014a
Daniel Gordon <danielgordon19972@gmail.com>
2022-11-13 09:28:38 +0200

    TEMP fake database

--- src/Components/menu-category/menu-category.js
+++ src/Components/menu-category/menu-category.js
@@ -7,13 +7,29 @@ const MenuCategory = ({ category }) => {
     const [response, setResponse] = useState([])
 
     useEffect(() => {
-            axios.get("https://princestavernapi.netlify.app/menu_items")
-                .then(res => {
-                    let arr = res.data.menu_items
-                    setResponse(arr)
-                })
+            // axios.get("https://princestavernapi.netlify.app/menu_items")
+            //     .then(res => {
+            //         let arr = res.data.menu_items
+                    // setResponse(arr)
+            // })
             // eslint-disable-next-line react-hooks/exhaustive-deps
-            }, [])
+            setResponse([
+                {title: "Cheese Burger", category: "Mains", description: "A very well made cheeseburger", ingrediants: 'bread, meat, pickels, lettuce, fried egg', url: "https://images.unsplash.com/photo-1571091718767-18b5b1457add?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YnVyZ2VyfGVufDB8MHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"},
+                {title: "Home Fries", category: "Sides", description: "The crunchiest home fries out there", ingrediants: 'potatoes, salt, olive oil, special seasoning, fried egg', url: "https://images.unsplash.com/photo-1585109649139-366815a0d713?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8ZnJpZXN8ZW58MHwwfDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"},
+                {title: "Side Salmon Salad", category: "Sides", description: "Side Salad", ingrediants: 'edomame, corn, cucumber, tomato, egg, salmon', url: "https://images.unsplash.com/photo-1546069901-ba9599a7e63c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2960&q=80"},
+                {title: "Pizza", category: "Mains", description: "A very well made cheeseburger", ingrediants: 'bread, meat, pickels, lettuce, fried egg', url: "https://images.unsplash.com/photo-1588315029754-2dd089d39a1a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1771&q=80"},
+                {title: "Buffalo Wings", category: "Sides", description: "The crunchiest home fries out there", ingrediants: 'potatoes, salt, olive oil, special seasoning, fried egg', url: "https://images.unsplash.com/photo-1602253057119-44d745d9b860?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8ZGlzaHxlbnwwfDB8MHx8&auto=format&fit=crop&w=800&q=60"},
+                {title: "Asparagus Salad", category: "Sides", description: "Fresh Edomame", ingrediants: 'edomame, salt', url: "https://images.unsplash.com/photo-1511994714008-b6d68a8b32a2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"},
+                {title: "Pork Chop", category: "Mains", description: "A very well made cheeseburger", ingrediants: 'bread, meat, pickels, lettuce, fried egg', url: "https://images.unsplash.com/photo-1432139555190-58524dae6a55?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1776&q=80"},
+                {title: "Shrimps", category: "Sides", description: "The crunchiest home fries out there", ingrediants: 'potatoes, salt, olive oil, special seasoning, fried egg', url: "https://images.unsplash.com/photo-1625943553852-781c6dd46faa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mjd8fG1lYWx8ZW58MHwwfDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"},
+                {title: "Eggplant Salad", category: "Sides", description: "Fresh Edomame", ingrediants: 'edomame, salt', url: "https://images.unsplash.com/photo-1604908177453-7462950a6a3b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1514&q=80"},
+                {title: "Shakshuka", category: "Mains", description: "A very well made cheeseburger", ingrediants: 'bread, meat, pickels, lettuce, fried egg', url: "https://images.unsplash.com/photo-1582492710145-d723e0a219f8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTAyfHxtZWFsfGVufDB8MHwwfHw%3D&auto=format&fit=crop&w=800&q=60"},
+                {title: "Vegetable Platter", category: "Sides", description: "The crunchiest home fries out there", ingrediants: 'potatoes, salt, olive oil, special seasoning, fried egg', url: "https://images.unsplash.com/photo-1561363268-f17491a094bf?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80"},
+                {title: "Okinawa soba", category: "Mains", description: "A very well made cheeseburger", ingrediants: 'bread, meat, pickels, lettuce, fried egg', url: "https://images.unsplash.com/photo-1568096889942-6eedde686635?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"},
+                {title: "Fried Rice", category: "Sides", description: "The crunchiest home fries out there", ingrediants: 'potatoes, salt, olive oil, special seasoning, fried egg', url: "https://images.unsplash.com/photo-1490990813269-10586274747f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1422&q=80"},
+                {title: "Mini Pizza", category: "Sides", description: "Fresh Edomame", ingrediants: 'edomame, salt', url: "https://images.unsplash.com/photo-1585995207653-dc16855299da?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"}
+            ])
+        }, [])
 
     return (
         <div className="menu-category">
--- src/Components/schedueled-events-grid/schedueled-events-grid.js
+++ src/Components/schedueled-events-grid/schedueled-events-grid.js
@@ -8,27 +8,32 @@ const SchedueledEventsGrid = () => {
     const [response, setResponse] = useState([])
 
     useEffect(() => {
-            axios.get("https://princestavernapi.netlify.app/events")
-                .then(res => {
-                    let firstArr = res.data.events
-                    let arr = []
-                    firstArr.forEach(obj => {
-                        console.log(obj)
-                        arr.push({
-                            date: new Date(parseInt(`${obj.date[0]}${obj.date[1]}${obj.date[2]}${obj.date[3]}`), parseInt(`${obj.date[5]}${obj.date[6]}`), parseInt(`${obj.date[8]}${obj.date[9]}`), parseInt(`${obj.start_time[0]}${obj.start_time[1]}`), parseInt(`${obj.start_time[3]}${obj.start_time[4]}`)),
-                            start_time: obj.start_time,
-                            end_time: obj.end_time,
-                            title: obj.title,
-                            description: obj.description,
-                            public: obj.public,
-                            url: obj.url,
-                            id: obj.id })
-                        })
-                        let sortedArr = arr.sort((obj1, obj2) => {
-                            return obj1.date - obj2.date
-                        })
-                        setResponse(sortedArr)
-                    })
+            // axios.get("https://princestavernapi.netlify.app/events")
+            //     .then(res => {
+            //         let firstArr = res.data.events
+            //         let arr = []
+            //         firstArr.forEach(obj => {
+            //             console.log(obj)
+            //             arr.push({
+            //                 date: new Date(parseInt(`${obj.date[0]}${obj.date[1]}${obj.date[2]}${obj.date[3]}`), parseInt(`${obj.date[5]}${obj.date[6]}`), parseInt(`${obj.date[8]}${obj.date[9]}`), parseInt(`${obj.start_time[0]}${obj.start_time[1]}`), parseInt(`${obj.start_time[3]}${obj.start_time[4]}`)),
+            //                 start_time: obj.start_time,
+            //                 end_time: obj.end_time,
+            //                 title: obj.title,
+            //                 description: obj.description,
+            //                 public: obj.public,
+            //                 url: obj.url,
+            //                 id: obj.id })
+            //             })
+            //             let sortedArr = arr.sort((obj1, obj2) => {
+            //                 return obj1.date - obj2.date
+            //             })
+            //             setResponse(sortedArr)
+            //         })
+            setResponse([{date: new Date(2020, 6, 8), title: "Singles Night", start_time: "8:00", end_time: "12:00", description: "We guarntee a 90% chance youre gonna get laid", public: true, url: "https://images.unsplash.com/flagged/photo-1574319523332-4cadaa531506?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mjd8fGJhciUyMHBhcnR5fGVufDB8MHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"},
+            {date: new Date(2020, 2, 20), title:"Trivia Night", start_time:"7:00", end_time: "10:00", description:"A great way to spend your night and meet new people", public: true, url: "https://images.unsplash.com/photo-1570937943292-a574bd5bc722?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8dHJpdmlhfGVufDB8MHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"},
+            {date: new Date(2020, 4, 1), title: "Couples Night", start_time: "10:00", end_time: "1:00", description: "If youre not sad and alone it gets better, now you get 50% off drinks too", public: true, url: "https://images.unsplash.com/photo-1542326891-50b14105a80b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fGNvdXBsZSUyMGJhcnxlbnwwfDB8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"},
+            {date: new Date(2020, 11, 11), title:"Bar Olympics", start_time:"9:00", end_time: "2:00", description:"A great night of bar competions like pool, darts and more. Winner gets free drinks for a night", public: true, url: "https://images.unsplash.com/photo-1566214999850-1698983935f3?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8YmFyJTIwZ2FtZXxlbnwwfDB8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"}
+            ])
             }, [])
 
     return (
